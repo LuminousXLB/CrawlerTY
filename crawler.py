db0c77876c78034609d5dd74460247b36c626c0d
Taraxacum <john980118@163.com>
2018-11-18 20:37:07 +0800

    dependency seperated

--- crawler.py
+++ crawler.py
@@ -203,36 +203,3 @@ def handlePost(blockid, postid):
                 meta['replys'] += handleFollowingPage(soup)
 
     return meta
-
-
-def wrapper(blockid, postid, times=0):
-    try:
-        meta = handlePost(blockid, postid)
-        if meta != None:
-            with open('data/{}_{}.json'.format(blockid, postid), 'w') as f:
-                json.dump(meta, f)
-    except requests.exceptions.ConnectionError as err:
-        logger.error(err.strerror)
-        if times < 3:
-            time.sleep(1+3*times)
-            return wrapper(blockid, postid, times+1)
-        else:
-            raise
-
-
-if __name__ == "__main__":
-    # wrapper('1179', 9176)
-    # for page in range(8789, 33150):
-    for page in range(20000, 28000):
-        wrapper('1179', page)
-
-    # for page in range(28000, 33150):
-    #     wrapper('1179', page)
-
-    # for page in range(20000, 24000):
-    #     wrapper('1179', page)
-
-    # for page in range(12000, 20000):
-    #     wrapper('1179', page)
-
-
