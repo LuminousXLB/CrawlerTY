43d088840cb0e2891b0e9c48a51e83021535ffb2
Taraxacum <john980118@163.com>
2018-12-04 23:22:24 +0800

    auto mkdir

--- settings.py
+++ settings.py
@@ -2,8 +2,12 @@ from pathlib import Path
 
 
 DATA_ROOT = Path('E:/RuntimeTY')
+
 DB_ENGINE_FILE = DATA_ROOT/'TYdata2.db'
 
 ECHO_DATABASE_INFO = False
 
 LOG_FILE = DATA_ROOT/'TYcrawler.log'
+
+if not DATA_ROOT.is_dir():
+    DATA_ROOT.mkdir()
