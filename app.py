d96ca75c29d6a4478280d016008f859322fe24a2
Taraxacum <john980118@163.com>
2018-12-06 15:22:35 +0800

    skip it

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ POST_RANGE = [
 ]
 
 RUNTIME_RANGE = [
-    ('develop', 2314119, 2329332),
+    ('develop', 2314236, 2329332),
     ('free', 5935881, 5970215),
     ('funinfo', 7666313, 7690308),
     ('worldlook', 1849540, 1859166)
@@ -17,6 +17,10 @@ RUNTIME_RANGE = [
 
 
 if __name__ == "__main__":
+    # [2018-12-05 13:04:25] CRITICAL| parser: Return 500 from GET http://bbs.tianya.cn/post-1179-14025-2.shtml
+    # [2018-12-05 18:37:24] CRITICAL| parser: Return 500 from GET http://bbs.tianya.cn/post-1179-17830-1.shtml
+    # handlePost('develop', 2314235) # Not Solved
+
     for blockid, start, end in RUNTIME_RANGE:
         for postid in range(start, end+1):
             handlePost(blockid, postid)
