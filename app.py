8362a23e8d58ebcfa897a881166cfd0465a9967e
Taraxacum <john980118@163.com>
2018-12-09 16:23:27 +0800

    ignore settings

--- app.py
+++ app.py
@@ -1,20 +1,5 @@
 from app_handle import handlePost
-
-POST_RANGE = [
-    ('1179', 8790, 33149),
-    ('develop', 2303713, 2329332),
-    ('free', 5935881, 5970215),
-    ('funinfo', 7666313, 7690308),
-    ('worldlook', 1849540, 1859166)
-]
-
-RUNTIME_RANGE = [
-    # ('1179', 8790, 33149),
-    ('develop', 2303713, 2329332),
-    ('free', 5935881, 5970215),
-    ('funinfo', 7666313, 7690308),
-    ('worldlook', 1849540, 1859166)
-]
+from settings import RUNTIME_RANGE
 
 
 if __name__ == "__main__":
